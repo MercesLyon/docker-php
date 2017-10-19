f298c8950574c190518680e813106bbbcb66fa07
Nicolas PHILIPPE <nikophil@gmail.com>
2017-10-19 17:32:16 +0200

    add xdebug conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,12 @@ chown -R www-data:www-data /var/www
 setfacl -R -m u:www-data:rwX -m u:`whoami`:rwX /var
 setfacl -dR -m u:www-data:rwX -m u:`whoami`:rwX /var
 
+echo "xdebug.remote_enable=On" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.profiler_enable=On" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.remote_host=${HOSTNAME:-upway.local}" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.idekey=${XDEBUG_IDE_KEY:-PHPSTORM}" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.remote_port=${XDEBUG_PORT:-9000}" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.remote_handler=dbgp" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.remote_connect_back=On" >> /usr/local/etc/php/conf.d/xdebug.ini
+
 exec "$@"
