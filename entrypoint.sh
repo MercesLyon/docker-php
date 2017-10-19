0a01253f4e5865c88f13ebbd40f08e962b958377
Guillaume Manen <g.manen@c2is.fr>
2017-10-19 14:43:43 +0200

    Fix permission /var/www

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ usermod -u ${WWWDATA_UID:-33} www-data
 groupmod -g ${WWWDATA_GID:-33} www-data
 
 chown -R www-data:www-data /var/.composer
-chown -R www-data:www-data /var/www/.composer /var/www/.ssh
+chown -R www-data:www-data /var/www
 
 setfacl -R -m u:www-data:rwX -m u:`whoami`:rwX /var
 setfacl -dR -m u:www-data:rwX -m u:`whoami`:rwX /var
