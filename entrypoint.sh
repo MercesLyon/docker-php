8b4081901a4578a051d5a5654b8fcc3eb3b865ae
Guillaume Manen <guillaume.manen@gmail.com>
2018-11-19 11:15:18 +0100

    Adding dockeruser

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-usermod -u ${WWWDATA_UID:-33} www-data
-groupmod -g ${WWWDATA_GID:-33} www-data
-
-chown -R www-data:www-data /var/.composer
-chown -R www-data:www-data /var/www
-
-setfacl -R -m u:www-data:rwX -m u:`whoami`:rwX /var
-setfacl -dR -m u:www-data:rwX -m u:`whoami`:rwX /var
-
 echo "xdebug.remote_enable=On" >> /usr/local/etc/php/conf.d/xdebug.ini \
     && echo "xdebug.profiler_enable=On" >> /usr/local/etc/php/conf.d/xdebug.ini \
     && echo "xdebug.remote_host=${HOSTNAME:-upway.local}" >> /usr/local/etc/php/conf.d/xdebug.ini \
